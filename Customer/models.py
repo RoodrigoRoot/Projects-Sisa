91165216cccd70a6a4f499e85ac9aee6d12be23a
Rodrigo <roodrigoroot@gmail.com>
2019-12-17 17:13:23 -0600

    Table customer Finished

--- Customer/models.py
+++ Customer/models.py
@@ -8,7 +8,8 @@ class Customer(models.Model):
     name_bussines = models.CharField(("Empresa"), blank=False, max_length=15)
     representative = models.CharField(("Representante"), max_length=50)
     phone = models.CharField(("Telefono"), max_length=15)
-
+    email = models.EmailField(max_length=200)
+    created_at = models.DateField(auto_now=True, auto_now_add=False)
     def __str__(self):
         return self.name_bussines
 
