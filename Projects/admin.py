8c6ebd9b91b3e58bb614b57f958db9e658929652
Rodrigo <roodrigoroot@gmail.com>
2020-01-03 16:00:03 -0600

    Proposal added

--- Projects/admin.py
+++ Projects/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
-from .models import Projects
+from .models import *
 # Register your models here.
 
 class ProjectsAdmin(admin.ModelAdmin):
 
     list_display=["name_project", "created_at", "modified_at", "slug"]
     fields = ("name_project", "bussines", "folio", "need", "comments")
-    ordering = ['name_project']
+    ordering = ['folio']
 
 admin.site.register(Projects, ProjectsAdmin)
+
+class ProposalAdmin(admin.ModelAdmin):
+    list_display=["project", "status", "created_at" ]
+
+admin.site.register(Proposal, ProposalAdmin)
--- Projects/models.py
+++ Projects/models.py
@@ -17,7 +17,7 @@ class Projects(models.Model):
     modified_at = models.DateTimeField(auto_now=False, auto_now_add=True) 
 
     def __str__(self):
-        return self.name_project
+        return "Proyecto: {} Folio: {}".format(self.name_project, self.folio)
     
     class Meta:
         verbose_name = "Proyecto"
@@ -29,3 +29,26 @@ def create_slug(sender, instance, **kwargs):
     instance.slug = slugify("{}-{}{}".format(instance.folio, instance.name_project, uid[:8]))
 
 pre_save.connect(create_slug, Projects)
+
+class Proposal(models.Model):
+    
+    STATUS = [
+        ("Pendiente", "Pendiente"),
+        ("Finalizada", "Finalizada"),
+        ("Con Cambios", "Com cambios"),
+        
+    ]
+
+    project = models.ForeignKey(Projects, verbose_name=("Proyecto"), on_delete=models.CASCADE)
+    limitations = models.TextField(("Limitaciones"))
+    requirements = models.TextField(("Requerimientos "))
+    justification = models.TextField(("Justificación"))
+    price = models.FloatField(("Precio Total"))
+    status = models.CharField(("Status"), max_length=50, choices=STATUS)
+    created_at = models.DateTimeField(auto_now=True, auto_now_add=False)
+    def __str__(self):
+        return str(self.project)
+
+    class Meta:
+        verbose_name = "Propuesta"
+        verbose_name_plural = "Propuestas"
